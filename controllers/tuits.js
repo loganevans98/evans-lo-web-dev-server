2275c051fbcb24a4bca98f62be3a497c84e89f8b
loganevans98 <loganevans98@gmail.com>
2022-04-05 20:57:20 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "handle": "NASA",
+        "likes": 345,
+        "dislikes": 200,
+        "comments": 0,
+        "retweets": 0,
+        "avatar": "../images/nasa.png",
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "handle": "NASA",
+        "likes": 346785,
+        "dislikes": 123,
+        "comments": 0,
+        "retweets": 0,
+        "avatar": "../images/nasa.png",
+    }
+];
+
